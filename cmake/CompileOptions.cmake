5593d883308ec8c9fe4fa0ade76a9a791021433d
Maksym Sditanov <msditanov@200volts.com>
2025-09-22 21:14:36 +0300

    Linux: add address sanitizer via libasan
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Maksym Sditanov <msditanov@200volts.com>

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -7,6 +7,7 @@ add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Woverloaded-virtual>)
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wuninitialized>)
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wmissing-declarations>)
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wstrict-aliasing>)
+add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wno-omit-frame-pointer>)
 
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/EHsc>)
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/W4>)
@@ -20,6 +21,16 @@ add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/we4289>) # loop variable 
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/w14905>) # wide string literal cast
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/w14906>) # string literal cast
 add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/w14928>) # illegal copy constructor/assignment
+
+if (ENABLE_ASAN)
+	add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-fsanitize=address>)
+	add_link_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-fsanitize=address>)
+
+	add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-fsanitize=undefined>)
+	add_link_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-fsanitize=undefined>)
+
+	add_link_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:-lasan>)
+endif()
 
 if (${FORCE_COLORED_OUTPUT})
     if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
